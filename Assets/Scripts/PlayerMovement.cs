a55c8a045694fcebebe1239a6983590a3c4ed5de
MahadAdnanGul <64431074+MahadAdnanGul@users.noreply.github.com>
2025-05-04 16:40:51 -0400

    removed extra files
    mostly from unity starter asset which i did not end up needing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Interfaces;
-using StarterAssets;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
